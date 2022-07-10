95d87ea8c404153966eb17d84e06594e724edf9a
mehmet <tas349873@gmail.com>
2022-07-10 21:55:52 +0300

    update

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,7 +1,8 @@
-import { FaEdit } from 'react-icons/fa';
-import { AiFillDelete } from 'react-icons/ai';
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import EditTutorial from "./EditTutorial";
 
-const TutorialList = ({tutorials,deleteTutorial,editTutorial}) => {
+const TutorialList = ({ tutorials, deleteTutorial, editTutorial }) => {
   //? Test data
   // let tutorials = [
   // ];
@@ -27,14 +28,26 @@ const TutorialList = ({tutorials,deleteTutorial,editTutorial}) => {
                 <td>{title}</td>
                 <td>{description}</td>
                 <td className="text-center">
-                  <FaEdit size={20} className="me-3 text-warning " onClick={()=>editTutorial(id,"","")} />
-                  <AiFillDelete size={22} className="text-danger cursor-pointer" onClick = {()=>deleteTutorial(id)}/>
+                  <FaEdit
+                    size={20}
+                    className="me-3 text-warning "
+                    onClick={() => editTutorial(id, "", "")}
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                  />
+
+                  <AiFillDelete
+                    size={22}
+                    className="text-danger cursor-pointer"
+                    onClick={() => deleteTutorial(id)}
+                  />
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      <EditTutorial />
     </div>
   );
 };
